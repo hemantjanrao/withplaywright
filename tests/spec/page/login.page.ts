6fdced57c8580ae8bec2ad43f1fbb240bc859b4a
Hemant Janrao <hemantjanrao@gmail.com>
2023-02-28 21:38:37 +0100

    generate artifacts

--- tests/spec/page/login.page.ts
+++ tests/spec/page/login.page.ts
@@ -2,36 +2,35 @@ import { Locator, Page } from '@playwright/test';
 import BasePage from '../../core/base/base.page';
 
 export default class LoginPage extends BasePage {
+  private inputEmail: Locator;
+  private inputPassword: Locator;
+  private buttonSubmit: Locator;
+
   constructor(page: Page) {
     super(page);
+    this.inputEmail = this.page.locator('#user-name');
+    this.inputPassword = this.page.locator('#password');
+    this.buttonSubmit = this.page.locator('#login-button');
   }
-
-  // Page locators
-  inputEmail = (): Locator => this.page.locator('#user-name');
-  inputPassword = (): Locator => this.page.locator('#password');
-  buttonSubmit = (): Locator => this.page.locator('#login-button');
 
   /**
    * Login to the application
    * @param  {string} username
    * @param  {string} password
-   * @returns Promise
+   * @returns Promise<void>
    */
   public async login(username: string, password: string): Promise<void> {
-    await this.inputEmail().fill(username);
-    await this.inputPassword().fill(password);
-    await this.buttonSubmit().click();
-  }
-
-  public async open(): Promise<void> {
-    this.page.goto('/');
+    this.page.goto("/");
+    await this.inputEmail.fill(username);
+    await this.inputPassword.fill(password);
+    await this.buttonSubmit.click();
   }
 
   /**
-   * Verify is the user on the
-   * @returns Promise
+   * Verify whether the user is on the login page
+   * @returns Promise<boolean>
    */
   public async isOn(): Promise<boolean> {
-    return await this.buttonSubmit().isVisible();
+    return await this.buttonSubmit.isVisible();
   }
 }
