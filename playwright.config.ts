343e5b4463b055a08647eff5040284046fb6ccc0
Hemant Janrao <hemantjanrao@gmail.com>
2023-02-28 22:32:43 +0100

    repo

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,13 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['./tests/core/listners/reporter.ts'], ['allure-playwright']],
+  reporter: [
+    ['./tests/core/listners/reporter.ts'], 
+    ["allure-playwright",{
+      detail: true,
+      outputFolder: "my-allure-results",
+      suiteTitle: false,}]
+    ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
