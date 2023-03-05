17b4c54cdfeeeede6a3cf27534a3a0ffb7adb5dd
Hemant Janrao <hemantjanrao@gmail.com>
2023-03-05 21:48:43 +0100

    updated

--- tests/spec/test.ts
+++ tests/spec/test.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { test, expect, request} from '@playwright/test';
+import { test, expect, request } from '@playwright/test';
 import { page, page_context } from '../core/base/base.test';
 import LoginPage from './page/login.page';
 
@@ -17,15 +17,15 @@ test.describe('Authentication', async () => {
 
   test('Authentication using API and then continue UI test', async () => {
     let response = await page.request.post('/parabank/login.htm', {
-    form:{
-      username:"h-janrao",
-      password:"@Test1234"
-    }});
-    
+      form: {
+        username: 'h-janrao',
+        password: '@Test1234'
+      }
+    });
+
     expect(response.status()).toBe(200);
 
-    await page.goto("/");
+    await page.goto('/');
     await expect(page.locator('li.home')).toBeVisible();
   });
-
 });
